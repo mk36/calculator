44d914844575e1dfb2021151cb8b06c07eb94928
mk36 <mikeking36@hotmail.co.uk>
2020-07-10 11:30:38 +0100

    created project with vue cli

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<style lang="scss" scoped>
+    .calculator-app{
+
+    }
+</style>
+
+<template>
+    <div class="calculator-app">
+       hello calculator
+    </div>
+</template>
+
+<script>
+    import "./styles/styles.scss";
+
+    export default {
+        name: 'App',
+        components: {
+
+        }
+    }
+</script>
